ffb61379d4bc2fb2286ef1abdf2c2de2b101e97d
Jérôme Mulsant <jerome@rue-de-la-vieille.fr>
2019-04-25 23:18:09 +0200

    GitLab CI correction

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -19,11 +19,11 @@ apt-get update -yqq
 
 # Install composer
 #apt-get install wget zlib1g-dev -yqq
-apt-get install zlib1g-dev unzip -yqq
+apt-get install git zlib1g-dev unzip -yqq
 docker-php-ext-install zip
-#wget https://composer.github.io/installer.sig -O - -q | tr -d '\n' > installer.sig
-#php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-#php -r "if (hash_file('SHA384', 'composer-setup.php') === file_get_contents('installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-#php composer-setup.php
-#php -r "unlink('composer-setup.php'); unlink('installer.sig');"
-composer install
+wget https://composer.github.io/installer.sig -O - -q | tr -d '\n' > installer.sig
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === file_get_contents('installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php'); unlink('installer.sig');"
+php composer.phar install

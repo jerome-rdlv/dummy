2beebb6eedb61398710782d784fdf10fcb315cde
Jérôme Mulsant <jerome@rue-de-la-vieille.fr>
2019-04-25 22:32:25 +0200

    GitLab CI correction

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -7,22 +7,23 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git -yqq
+#apt-get install git -yqq
 
 # Install phpunit, the tool that we will use for testing
-curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
-chmod +x /usr/local/bin/phpunit
+#curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
+#chmod +x /usr/local/bin/phpunit
 
 # Install mysql driver
 # Here you can install any other extension that you need
-docker-php-ext-install pdo_mysql
+#docker-php-ext-install pdo_mysql
 
 # Install composer
-apt-get install wget zlib1g-dev -yqq
+#apt-get install wget zlib1g-dev -yqq
+apt-get install zlib1g-dev -yqq
 docker-php-ext-install zip
-wget https://composer.github.io/installer.sig -O - -q | tr -d '\n' > installer.sig
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === file_get_contents('installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php'); unlink('installer.sig');"
+#wget https://composer.github.io/installer.sig -O - -q | tr -d '\n' > installer.sig
+#php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+#php -r "if (hash_file('SHA384', 'composer-setup.php') === file_get_contents('installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+#php composer-setup.php
+#php -r "unlink('composer-setup.php'); unlink('installer.sig');"
 php composer.phar install
